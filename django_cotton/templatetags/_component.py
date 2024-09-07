278bbfed5a2925e39484306d60a2987713c27cf0
Will Abbott <will@digitaloutback.co.uk>
2024-09-07 09:37:13 +0100

    started investigation

--- django_cotton/templatetags/_component.py
+++ django_cotton/templatetags/_component.py
@@ -2,9 +2,9 @@ import ast
 
 from django import template
 from django.conf import settings
-from django.template import Node, Template, Context
 from django.utils.safestring import mark_safe
 from django.template.loader import get_template
+from django.template import Node, Template, Context
 
 from django_cotton.utils import ensure_quoted
 
@@ -51,41 +51,41 @@ class CottonComponentNode(Node):
         attrs = self._build_attrs(context)
 
         # Add the remainder as the default slot
-        local_ctx = context.flatten()
-        local_ctx["slot"] = self.nodelist.render(context)
+        ctx = context.flatten()
+        ctx["slot"] = self.nodelist.render(context)
 
         # Merge slots and attributes into the local context
         all_named_slots_ctx = context.get("cotton_named_slots", {})
-        local_named_slots_ctx = all_named_slots_ctx.get(self.component_key, {})
-        local_ctx.update(local_named_slots_ctx)
+        named_slots_ctx = all_named_slots_ctx.get(self.component_key, {})
+        ctx.update(named_slots_ctx)
 
         # We need to check if any dynamic attributes are present in the component slots, process them and move them over to attrs
-        if "ctn_template_expression_attrs" in local_named_slots_ctx:
-            for expression_attr in local_named_slots_ctx["ctn_template_expression_attrs"]:
+        if "ctn_template_expression_attrs" in named_slots_ctx:
+            for expression_attr in named_slots_ctx["ctn_template_expression_attrs"]:
                 # Process them like a non-extracted attribute
                 if expression_attr[0] == ":":
                     evaluated = self._process_dynamic_attribute(
-                        local_named_slots_ctx[expression_attr], local_ctx
+                        named_slots_ctx[expression_attr], ctx
                     )
                     expression_attr = expression_attr[1:]
                     attrs[expression_attr] = evaluated
                 else:
-                    attrs[expression_attr] = local_named_slots_ctx[expression_attr]
+                    attrs[expression_attr] = named_slots_ctx[expression_attr]
 
         attrs_string = " ".join(f"{key}={ensure_quoted(value)}" for key, value in attrs.items())
-        local_ctx["attrs"] = mark_safe(attrs_string)
-        local_ctx["attrs_dict"] = attrs
+        ctx["attrs"] = mark_safe(attrs_string)
+        ctx["attrs_dict"] = attrs
 
         # Ensure attributes are accessible, eg. 'x-init' -> {{ x_init }}
         attrs = {key.replace("-", "_"): value for key, value in attrs.items()}
-        local_ctx.update(attrs)
+        ctx.update(attrs)
 
         # Reset the component's slots in context to prevent data leaking between components.
         all_named_slots_ctx[self.component_key] = {}
 
         template_path = self._generate_component_template_path(attrs)
 
-        return get_template(template_path).render(local_ctx)
+        return get_template(template_path).render(ctx)
 
     def _build_attrs(self, context):
         """
@@ -114,7 +114,8 @@ class CottonComponentNode(Node):
         """
         # We might be passing a variable by reference
         try:
-            return template.Variable(value).resolve(context)
+            value = template.Variable(value).resolve(context)
+            return value
         except (template.VariableDoesNotExist, IndexError):
             pass
 
